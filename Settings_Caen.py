ab932e3ab607cecc966c36fb9b549d3d3144e9c7
Gregor Kasieczka <g.kasieczka@gmail.com>
2019-08-01 16:38:51 +0200

    fixed typo. Entered settings for test with 3 out of 4 signal scintillators at roughly 1900V.

--- Settings_Caen.py
+++ Settings_Caen.py
@@ -165,8 +165,8 @@ class Settings_Caen:
 						self.veto_offset = self.veto_polarity * 45
 
 				if parser.has_section('OUTPUT'):
-					if parser.has_option('OUTPUT', 'inDir'):
-						self.outdir = parser.get('OUTPUT', 'inDir')
+					if parser.has_option('OUTPUT', 'dir'):
+						self.outdir = parser.get('OUTPUT', 'dir')
 					if parser.has_option('OUTPUT', 'prefix'):
 						self.prefix = parser.get('OUTPUT', 'prefix')
 					if parser.has_option('OUTPUT', 'suffix'):
